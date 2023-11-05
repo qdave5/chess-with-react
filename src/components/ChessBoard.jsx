aa610de701f64fc338987d31ddbc3d05cc8c5923
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2023-11-05 07:47:20 +0700

    add tile labeling

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 import { Col, Container, Row } from "reactstrap";
 import ChessBoardTile from "./ChessBoardTile";
+import { getColumnName } from "../functions";
 
 const ChessBoard = ({ tileItems }) => {
+  let rowInt = 0;
+  let colInt = 9;
   return (
     <div>
-      <Container fluid className="d-flex justify-content-center w-75 h-75">
+      <Container fluid className="d-flex justify-content-center w-75">
         <Row className="align-items-center">
-          {tileItems.map((row) => (
-            <>
-              {row.map((cell) => {
-                return (
-                  <Col style={{ width: "12.5%" }}>
-                    <ChessBoardTile piece={cell} />
-                  </Col>
-                );
-              })}
-              <div className="w-100"></div>
-            </>
-          ))}
+          {tileItems.map((row) => {
+            colInt--;
+            rowInt = 0;
+            return (
+              <>
+                {row.map((cell) => {
+                  rowInt++;
+                  return (
+                    <Col style={{ width: "12.5%" }}>
+                      <ChessBoardTile
+                        piece={cell}
+                        tileLocation={`${colInt}${getColumnName(rowInt)}`}
+                      />
+                    </Col>
+                  );
+                })}
+                <div className="w-100"></div>
+              </>
+            );
+          })}
         </Row>
       </Container>
     </div>
--- src/components/ChessBoardTile.jsx
+++ src/components/ChessBoardTile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
 
-const ChessBoardTile = ({ piece }) => {
+const ChessBoardTile = ({ piece, tileLocation }) => {
   return (
     <Button
       className="w-100 h-100"
       onClick={(e) => {
         e.preventDefault();
-        console.log(piece.location || "clicked");
+        console.log(`${tileLocation} is clicked`);
       }}
     >
       <p>{piece.image || " "}</p>
