ac3af12ab246e4d94580ab4991d029e5c2ea7ff7
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2023-11-25 23:23:38 +0700

    refactor isValidMove + separate pawnValidMove

--- src/components/ChessBoard/ChessBoard.jsx
+++ src/components/ChessBoard/ChessBoard.jsx
@@ -4,6 +4,7 @@ import ChessBoardTile from "./ChessBoardTile";
 import { checkValidMove } from "../../functions/PieceMovement/BasicMovement";
 import { getIndexFromRowCol } from "../../functions";
 import { PieceSide } from "../../constant/PieceSide";
+import { isValidMove } from "../../functions/PieceMovement/isValidMove";
 
 const ChessBoard = ({ tileItems, sourcePiece, sideTurn, handleClickEvent }) => {
   const [tile8x8, setTile8x8] = useState([]);
@@ -54,31 +55,10 @@ const isButtonActive = (tileItems, sourcePiece, cell, sideTurn) => {
     : sourcePiece === cell ||
         isValidMove(
           tileItems,
-          sideTurn,
+          sourcePiece,
           checkValidMove(sourcePiece),
           getIndexFromRowCol(cell.row, cell.col)
         );
-};
-
-const isValidMove = (tileItems, sideTurn, validMove, index) => {
-  const listValidMove = [];
-  validMove.map((listIdx) => {
-    let listTile = [];
-    for (var idx = 0; idx < listIdx.length; idx++) {
-      if (tileItems[listIdx[idx]]?.piece.side === PieceSide.Empty) {
-        listTile.push(listIdx[idx]);
-      } else if (tileItems[listIdx[idx]]?.piece.side === sideTurn) {
-        break;
-      } else {
-        listTile.push(listIdx[idx]);
-        break;
-      }
-    }
-
-    listValidMove.push(listTile);
-  });
-
-  return listValidMove.flat().includes(index);
 };
 
 export default ChessBoard;
---
+++ src/functions/PieceMovement/isValidMove.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { PieceSide } from "../../constant/PieceSide";
+import PieceType from "../../constant/PieceType";
+
+export const isValidMove = (tileItems, source, validMove, index) => {
+  const listValidMove = [];
+
+  if (source?.piece.type === PieceType.Pawn) {
+    listValidMove.push(isValidMovePawn(tileItems, source, validMove));
+  } else {
+    listValidMove.push(isValidMoveOther(tileItems, source, validMove));
+  }
+
+  console.log(listValidMove.flat());
+
+  return listValidMove.flat().includes(index);
+};
+
+const isValidMovePawn = (tileItems, source, validMove) => {
+  let listTile = [];
+
+  const idxFront = validMove[0];
+  const idxOther = [validMove[1], validMove[2]].flat();
+  console.log(idxFront, idxOther);
+
+  // front
+  for (var idx = 0; idx < 2; idx++) {
+    if (tileItems[idxFront[idx]]?.piece.side === PieceSide.Empty) {
+      listTile.push(idxFront[idx]);
+    }
+  }
+
+  // front-right / front-left
+  for (var idx = 0; idx < 2; idx++) {
+    if (
+      tileItems[idxOther[idx]]?.piece.side !== source?.piece.side &&
+      tileItems[idxOther[idx]]?.piece.side !== PieceSide.Empty
+    ) {
+      listTile.push(idxOther[idx]);
+    }
+  }
+
+  return listTile;
+};
+
+const isValidMoveOther = (tileItems, source, validMove) =>
+  validMove
+    .map((listIdx) => {
+      let listTile = [];
+      for (var idx = 0; idx < listIdx.length; idx++) {
+        if (tileItems[listIdx[idx]]?.piece.side === PieceSide.Empty) {
+          listTile.push(listIdx[idx]);
+        } else if (tileItems[listIdx[idx]]?.piece.side === source?.piece.side) {
+          break;
+        } else {
+          listTile.push(listIdx[idx]);
+          break;
+        }
+      }
+
+      return listTile;
+    })
+    .flat();
