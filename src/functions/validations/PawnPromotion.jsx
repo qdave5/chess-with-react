fd0c929780d84a6226ca52e6df6745fb169e85f0
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2023-12-03 21:19:21 +0700

    fix validation for not working

--- src/functions/validations/PawnPromotion.jsx
+++ src/functions/validations/PawnPromotion.jsx
@@ -3,5 +3,7 @@ import PieceType from "../../constant/PieceType";
 export const isPawnMoveToTop = (sourceTile, targetTile) => {
   console.log("source", sourceTile);
   console.log("target", targetTile);
-  return sourceTile.piece.type === PieceType.Pawn && targetTile.row in [1, 8];
+  return (
+    sourceTile.piece.type === PieceType.Pawn && [1, 8].includes(targetTile.row)
+  );
 };
