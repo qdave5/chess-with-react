3a18ff0eae8101d5eae7c58961ab75fa16d8ab7c
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2023-12-06 13:21:16 +0700

    implement Rook move in castling

--- src/functions/PieceMovement/BasicMovement.jsx
+++ src/functions/PieceMovement/BasicMovement.jsx
@@ -3,7 +3,7 @@ import { EmptyPiece } from "../../components/Piece";
 import { PieceSide } from "../../constant/PieceSide";
 import PieceType from "../../constant/PieceType";
 import { getTile } from "../Tiles";
-import { isPawnType, isWhiteSide } from "../validations/PieceCheck";
+import { isKingType, isPawnType, isWhiteSide } from "../validations/PieceCheck";
 import { pieceStep } from "./PieceStep";
 import { stepDown, stepUp } from "./Step";
 
@@ -16,23 +16,42 @@ export const movePiece = (sourceTile, targetTile, tileList, totalStep) => {
 
   // en passant
   enPassant(targetTile, tileList);
+
+  // castling
+  castling(sourceTile, targetTile, tileList);
 };
 
-const enPassant = (targetTile, tileList) => {
-  if (isPawnType(targetTile)) {
+const enPassant = (afterTile, tileList) => {
+  if (isPawnType(afterTile)) {
     let addRow = 0;
 
-    if (isWhiteSide(targetTile)) addRow = 1;
+    if (isWhiteSide(afterTile)) addRow = 1;
     else addRow = -1;
 
     const behindPiece = getTile(
       tileList,
-      targetTile.row + addRow + "-" + targetTile.col
+      afterTile.row + addRow + "-" + afterTile.col
     );
 
     if (isPawnType(behindPiece)) {
       behindPiece.piece = EmptyPiece();
     }
+  }
+};
+
+const castling = (beforeTile, afterTile, tileList) => {
+  if (isKingType(afterTile)) {
+    const beforeRookTile = getTile(
+      tileList,
+      afterTile.row + "-" + (afterTile.col < beforeTile.col ? 1 : 8)
+    );
+    const afterRookTile = getTile(
+      tileList,
+      afterTile.row + "-" + (beforeTile.col + afterTile.col) / 2
+    );
+
+    afterRookTile.piece = beforeRookTile.piece;
+    beforeRookTile.piece = EmptyPiece();
   }
 };
 
